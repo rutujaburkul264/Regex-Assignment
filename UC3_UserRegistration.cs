b699be1dedff8e86efa940545054fa773f408e4f
rutujaburkul264 <rutujaburkul264@gmail.com>
2021-10-05 23:34:00 +0530

    valid email

--- UC3_UserRegistration.cs
+++ UC3_UserRegistration.cs
@@ -7,9 +7,9 @@ namespace UserRegistrationProblem
     class UC3_UserRegistration
     {
         string Regex_Pincode = "^[a-zA-Z0-9]+([.#_$+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
-        public bool validatePinCode(string pincode)
+        public bool validatePinCode(string email)
         {
-            return Regex.IsMatch(pincode, Regex_Pincode);
+            return Regex.IsMatch(email, Regex_Pincode);
         }
 
     }
