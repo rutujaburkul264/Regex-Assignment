3055f34a7a2749c9838c89da28b8b0e42f0255af
rutujaburkul264 <rutujaburkul264@gmail.com>
2021-10-06 19:29:07 +0530

    Email Samples

---
+++ UC9_UserRegistration.cs
@@ -0,0 +1,15 @@
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+    class UC9_UserRegistration
+    {
+        string Regex_Pincode = "^[a-zA-Z0-9]+([.#_$+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
+        public bool validatePinCode(string email)
+        {
+            return Regex.IsMatch(email, Regex_Pincode);
+        }
+    }
+}
