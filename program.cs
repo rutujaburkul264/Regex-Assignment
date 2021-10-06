9f1c1ac48cede59ff9b6a7b821eccc94a2c64117
rutujaburkul264 <rutujaburkul264@gmail.com>
2021-10-06 19:32:54 +0530

    Program.cs File

--- program.cs
+++ program.cs
@@ -14,7 +14,16 @@ namespace UserRegistrationProblem
 	    // Console.WriteLine(patterns.validatePinCode("rutuja12"));
             // Console.WriteLine(patterns.validatePinCode("Rutuja12"));
             // Console.WriteLine(patterns.validatePinCode("Rutuja89"));
-             Console.WriteLine(patterns.validatePinCode("Rutuja@9"));
+            // Console.WriteLine(patterns.validatePinCode("Rutuja@9"));
+	     Console.WriteLine(patterns.validatePinCode("abc@yahoo.com"));
+            Console.WriteLine(patterns.validatePinCode("abc-100@yahoo.com"));
+            Console.WriteLine(patterns.validatePinCode("abc.100@yahoo.com"));
+            Console.WriteLine(patterns.validatePinCode("abc111@abc.com"));
+            Console.WriteLine(patterns.validatePinCode("abc-100@abc.net"));
+            Console.WriteLine(patterns.validatePinCode("abc.100@abc.com.au"));
+            Console.WriteLine(patterns.validatePinCode("abc@1.com"));
+            Console.WriteLine(patterns.validatePinCode("abc+100@gmail.com"));
+	     
             
             Console.ReadKey();
         }
